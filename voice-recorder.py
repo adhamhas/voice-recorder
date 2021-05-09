92c0af9b6a61761f8ca7107735f75cdf08f9c7d4
adhamhas <56196111+adhamhas@users.noreply.github.com>
2021-05-09 18:52:32 +0000

    Create voice-recorder.py

---
+++ voice-recorder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sounddevice
+from scipy.io.wavfile import write
+
+fs=44100
+second= 50
+print("recording...")
+record_voice=sounddevice.rec(int(second * fs),samplerate=fs,channels=2)
+sounddevice.wait()
+
+write("output.wav",fs,record_voice)
